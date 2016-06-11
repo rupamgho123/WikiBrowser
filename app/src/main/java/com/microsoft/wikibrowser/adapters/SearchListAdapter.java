722958dd5ac32d085e79fc0e392a45b73a8b8f4d
Rupam Ji <rupam.ghosh@flipkart.com>
2016-06-12 00:56:55 +0530

    working prototype

---
+++ app/src/main/java/com/microsoft/wikibrowser/adapters/SearchListAdapter.java
@@ -0,0 +1,65 @@
+package com.microsoft.wikibrowser.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.microsoft.wikibrowser.R;
+import com.microsoft.wikibrowser.models.Page;
+import com.microsoft.wikibrowser.models.SearchResponse;
+import com.squareup.picasso.Picasso;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by rupam.ghosh on 11/06/16.
+ */
+public class SearchListAdapter extends RecyclerView.Adapter<SearchListAdapter.WikiPageHolder> {
+
+  SearchResponse searchResponse;
+  List<Page> pages;
+  Context context;
+  public SearchListAdapter(SearchResponse searchResponse,Context context){
+    this.searchResponse = searchResponse;
+    this.context = context;
+    pages = new ArrayList<>();
+    pages.addAll(searchResponse.getQuery().getPages().values());
+  }
+
+  @Override public WikiPageHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    return new WikiPageHolder(LayoutInflater.from(parent.getContext())
+        .inflate(R.layout.list_item, parent, false));
+  }
+
+  @Override public void onBindViewHolder(WikiPageHolder holder, int position) {
+    Page page = pages.get(position);
+    holder.titleView.setText(page.getTitle());
+    if(page.getThumbnail() != null) {
+      Picasso.with(context).
+          load(page.getThumbnail().getSource()).
+          placeholder(R.drawable.placeholder).
+          into(holder.thumbnailView);
+    }
+    else {
+      holder.thumbnailView.setImageResource(R.drawable.placeholder);
+    }
+  }
+
+  @Override public int getItemCount() {
+    return pages.size();
+  }
+
+  public static class WikiPageHolder extends RecyclerView.ViewHolder{
+    ImageView thumbnailView;
+    TextView titleView;
+
+    public WikiPageHolder(View itemView) {
+      super(itemView);
+      titleView = (TextView) itemView.findViewById(R.id.title);
+      thumbnailView = (ImageView) itemView.findViewById(R.id.imgView);
+    }
+  }
+}
--- app/src/main/java/com/microsoft/wikibrowser/fragments/SearchListFragment.java
+++ app/src/main/java/com/microsoft/wikibrowser/fragments/SearchListFragment.java
@@ -1,12 +1,17 @@
 package com.microsoft.wikibrowser.fragments;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
+import butterknife.Bind;
+import com.microsoft.wikibrowser.adapters.SearchListAdapter;
 import com.microsoft.wikibrowser.events.SearchQueryRequestedEvent;
 import com.microsoft.wikibrowser.models.SearchResponse;
 import com.microsoft.wikibrowser.providers.WikiServiceProvider;
@@ -21,6 +26,8 @@ import com.microsoft.wikibrowser.R;
  */
 public class SearchListFragment extends BaseFragment implements Callback<SearchResponse>{
   WikiServiceProvider wikiServiceProvider = new WikiServiceProvider();
+  ProgressDialog progressDialog;
+  @Bind(R.id.recycler_view) RecyclerView recyclerView;
 
   @Nullable @Override
   public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
@@ -28,18 +35,30 @@ public class SearchListFragment extends BaseFragment implements Callback<SearchR
     return inflater.inflate(R.layout.list,container,false);
   }
 
+  @Override public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    progressDialog = new ProgressDialog(getContext());
+    progressDialog.setMessage("Loading...");
+  }
+
   @Subscribe
   public void onSearchQueryRequested(SearchQueryRequestedEvent event){
     Call<SearchResponse> searchCall = wikiServiceProvider.get().search("query","pageimages","json",
         "thumbnail",50,50,"prefixsearch",event.getQuery());
     searchCall.enqueue(this);
+    progressDialog.show();
   }
 
   @Override public void onResponse(Call<SearchResponse> call, Response<SearchResponse> response) {
-
+    SearchListAdapter searchListAdapter = new SearchListAdapter(response.body(), getContext());
+    recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+    recyclerView.setAdapter(searchListAdapter);
+    searchListAdapter.notifyDataSetChanged();
+    progressDialog.hide();
   }
 
   @Override public void onFailure(Call<SearchResponse> call, Throwable t) {
-    Toast.makeText(getContext(),"Could not fetch data from wikipedia",Toast.LENGTH_SHORT).show();
+    Toast.makeText(getContext(),"Could not fetch data from wikipedia",Toast.LENGTH_LONG).show();
+    progressDialog.hide();
   }
 }
